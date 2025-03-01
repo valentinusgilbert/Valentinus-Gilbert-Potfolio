6e78a0397ff50720a75042e60ffded520b285283
valentinusgilbert <valentinus.sanjaya@gdn-commerce.com>
2025-03-01 12:21:45 +0700

    add music

--- portfolio/src/js/app.js
+++ portfolio/src/js/app.js
@@ -1,15 +1,36 @@
 import Header from '@/components/Header.vue';
 import Banner from '@/components/Banner.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { ref } from 'vue';
+import music from '@/assets/music/music.mp3';
+
+library.add(faPlay, faPause);
 
 export default {
     components: {
         Header,
-        Banner
+        Banner,
+        FontAwesomeIcon
     },
     setup() {
-        
-        return{
+        const isPlaying = ref(false);
+        const audio = new Audio(music);
 
+        const togglePlay = () => {
+            if (isPlaying.value) {
+                audio.pause();
+                isPlaying.value = false;
+            } else {
+                audio.play();
+                isPlaying.value = true;
+            }
+        };
+
+        return {
+            isPlaying,
+            togglePlay
         }
     }
 }
